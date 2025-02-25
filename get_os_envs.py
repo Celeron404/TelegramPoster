5cd1a1d954dbaecae7a753be4f705ce887bf1b95
Сергей Зимацкий <ZimatskiiSN@ekassir.com>
2025-02-25 19:01:00 +0400

    minor language fix

--- get_os_envs.py
+++ get_os_envs.py
@@ -15,14 +15,14 @@ def load_envs():
         try:
             api_id = int(api_id_str)
         except ValueError:
-            logger.error("Ошибка: api_id_str не является целым числом.")
+            logger.error("Error: api_id_str is not int.")
     else:
-        raise ValueError("TELEGRAM_API_ID не задан.")
+        raise ValueError("TELEGRAM_API_ID was not set.")
     env_dict["TELEGRAM_API_ID"] = api_id
 
     api_hash = os.getenv("TELEGRAM_API_HASH", "NONE")
     if api_hash == "NONE":
-        raise ValueError("TELEGRAM_API_HASH не задан.")
+        raise ValueError("TELEGRAM_API_HASH was not set.")
     env_dict["TELEGRAM_API_HASH"] = api_hash
 
     main_dialog_id = None
@@ -31,9 +31,9 @@ def load_envs():
         try:
             main_dialog_id = int(main_dialog_id_str)
         except ValueError:
-            logger.error("Ошибка: main_dialog_id_str не является целым числом.")
+            logger.error("Error: main_dialog_id_str is not int.")
     else:
-        raise ValueError("MAIN_DIALOG_ID не задан.")
+        raise ValueError("MAIN_DIALOG_ID was not set.")
     env_dict["MAIN_DIALOG_ID"] = main_dialog_id
 
     dialogs_to_post_ids = None
@@ -42,9 +42,9 @@ def load_envs():
         try:
             dialogs_to_post_ids = list(map(int, dialogs_to_post_ids_str))
         except ValueError:
-            logger.error("Ошибка: dialogs_to_post_ids_str содержит не только целые числа или не содержит целые числа.")
+            logger.error("Error: dialogs_to_post_ids_str not contains int or contains not only int.")
     else:
-        raise ValueError("DIALOGS_TO_POST_IDS не задан.")
+        raise ValueError("DIALOGS_TO_POST_IDS was not set.")
     env_dict["DIALOGS_TO_POST_IDS"] = dialogs_to_post_ids
 
     sleep_between_send_messages = None
@@ -53,9 +53,9 @@ def load_envs():
         try:
             sleep_between_send_messages = int(sleep_between_send_messages_str)
         except ValueError:
-            logger.error("Ошибка: SLEEP_BETWEEN_SEND_MESSAGES не является целым числом.")
+            logger.error("Error: SLEEP_BETWEEN_SEND_MESSAGES is not int.")
     else:
-        raise ValueError("SLEEP_BETWEEN_SEND_MESSAGES не задан.")
+        raise ValueError("SLEEP_BETWEEN_SEND_MESSAGES was not set.")
     env_dict["SLEEP_BETWEEN_SEND_MESSAGES"] = sleep_between_send_messages
 
     return env_dict
